575654ce804d4e635c7dbe3dae6d3d9f1a77bf17
Jaycadox <>
2024-01-06 22:52:06 +1000

    factor input utilities to seperate source file

---
+++ src/input_helper.rs
@@ -0,0 +1,56 @@
+use macroquad::{input::get_last_key_pressed, miniquad::KeyCode};
+
+pub enum InputAction {
+    NumberEntered(u8),
+    Function(u8),
+    Reset,
+    Clear,
+    AutoPlay,
+    MoveUp,
+    MoveDown,
+    MoveLeft,
+    MoveRight,
+}
+
+impl TryFrom<KeyCode> for InputAction {
+    type Error = String;
+    fn try_from(value: KeyCode) -> Result<Self, Self::Error> {
+        Ok(match value {
+            KeyCode::Key1 => InputAction::NumberEntered(1),
+            KeyCode::Key2 => InputAction::NumberEntered(2),
+            KeyCode::Key3 => InputAction::NumberEntered(3),
+            KeyCode::Key4 => InputAction::NumberEntered(4),
+            KeyCode::Key5 => InputAction::NumberEntered(5),
+            KeyCode::Key6 => InputAction::NumberEntered(6),
+            KeyCode::Key7 => InputAction::NumberEntered(7),
+            KeyCode::Key8 => InputAction::NumberEntered(8),
+            KeyCode::Key9 => InputAction::NumberEntered(9),
+            KeyCode::Backspace | KeyCode::Key0 | KeyCode::Delete => InputAction::Clear,
+            KeyCode::Space => InputAction::AutoPlay,
+            KeyCode::Tab => InputAction::Reset,
+            KeyCode::F1 => InputAction::Function(1),
+            KeyCode::F2 => InputAction::Function(2),
+            KeyCode::F3 => InputAction::Function(3),
+            KeyCode::F4 => InputAction::Function(4),
+            KeyCode::F5 => InputAction::Function(5),
+            KeyCode::F6 => InputAction::Function(6),
+            KeyCode::F7 => InputAction::Function(7),
+            KeyCode::F8 => InputAction::Function(8),
+            KeyCode::F9 => InputAction::Function(9),
+            KeyCode::F10 => InputAction::Function(10),
+            KeyCode::F11 => InputAction::Function(11),
+            KeyCode::F12 => InputAction::Function(12),
+            KeyCode::W | KeyCode::Up => InputAction::MoveUp,
+            KeyCode::A | KeyCode::Left => InputAction::MoveLeft,
+            KeyCode::S | KeyCode::Down => InputAction::MoveDown,
+            KeyCode::D | KeyCode::Right => InputAction::MoveRight,
+            _ => Err("Not a recognised key".to_string())?,
+        })
+    }
+}
+
+impl InputAction {
+    pub fn get_last_input() -> Option<InputAction> {
+        get_last_key_pressed().and_then(|key| InputAction::try_from(key).ok())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,18 @@
-use std::collections::HashSet;
-
-use draw_helper::*;
-use macroquad::miniquad::window::screen_size;
-use macroquad::prelude::*;
 mod draw_helper;
+mod input_helper;
 mod status_bar;
 mod sudoku_game;
 mod task_status;
+
+use draw_helper::*;
+use input_helper::*;
+use macroquad::miniquad::window::screen_size;
+use macroquad::prelude::*;
 use status_bar::cpu_solver::*;
 use status_bar::fps::Fps;
 use status_bar::{StatusBar, StatusBarItemStatus};
+use std::collections::HashSet;
 use sudoku_game::SudokuGame;
-
-enum InputAction {
-    NumberEntered(u8),
-    Function(u8),
-    Reset,
-    Clear,
-    AutoPlay,
-    MoveUp,
-    MoveDown,
-    MoveLeft,
-    MoveRight,
-}
-
-impl TryFrom<KeyCode> for InputAction {
-    type Error = String;
-    fn try_from(value: KeyCode) -> Result<Self, Self::Error> {
-        Ok(match value {
-            KeyCode::Key1 => InputAction::NumberEntered(1),
-            KeyCode::Key2 => InputAction::NumberEntered(2),
-            KeyCode::Key3 => InputAction::NumberEntered(3),
-            KeyCode::Key4 => InputAction::NumberEntered(4),
-            KeyCode::Key5 => InputAction::NumberEntered(5),
-            KeyCode::Key6 => InputAction::NumberEntered(6),
-            KeyCode::Key7 => InputAction::NumberEntered(7),
-            KeyCode::Key8 => InputAction::NumberEntered(8),
-            KeyCode::Key9 => InputAction::NumberEntered(9),
-            KeyCode::Backspace | KeyCode::Key0 | KeyCode::Delete => InputAction::Clear,
-            KeyCode::Space => InputAction::AutoPlay,
-            KeyCode::Tab => InputAction::Reset,
-            KeyCode::F1 => InputAction::Function(1),
-            KeyCode::F2 => InputAction::Function(2),
-            KeyCode::F3 => InputAction::Function(3),
-            KeyCode::F4 => InputAction::Function(4),
-            KeyCode::F5 => InputAction::Function(5),
-            KeyCode::F6 => InputAction::Function(6),
-            KeyCode::F7 => InputAction::Function(7),
-            KeyCode::F8 => InputAction::Function(8),
-            KeyCode::F9 => InputAction::Function(9),
-            KeyCode::F10 => InputAction::Function(10),
-            KeyCode::F11 => InputAction::Function(11),
-            KeyCode::F12 => InputAction::Function(12),
-            KeyCode::W | KeyCode::Up => InputAction::MoveUp,
-            KeyCode::A | KeyCode::Left => InputAction::MoveLeft,
-            KeyCode::S | KeyCode::Down => InputAction::MoveDown,
-            KeyCode::D | KeyCode::Right => InputAction::MoveRight,
-            _ => Err("Not a recognised key".to_string())?,
-        })
-    }
-}
 
 fn draw_sudoku(game: &mut SudokuGame, drawing: &DrawingSettings, status_bar: &mut StatusBar) {
     let (mut width, mut height) = screen_size();
@@ -94,7 +47,7 @@ fn draw_sudoku(game: &mut SudokuGame, drawing: &DrawingSettings, status_bar: &mu
         _ => vec![],
     };
 
-    let mut key = get_last_key_pressed().and_then(|key| InputAction::try_from(key).ok());
+    let mut key = InputAction::get_last_input();
 
     if let Some((mx, my)) = mouse_pos {
         let mut change_selected_to_cursor = false;
