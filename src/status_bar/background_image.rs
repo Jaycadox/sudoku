e7ef42b6a9e5ad3953e8f1ace165f01ab3f32177
Jaycadox <jaycadox@example.com>
2024-01-16 07:29:18 +1000

    remove http/s backgroundimage source

--- src/status_bar/background_image.rs
+++ src/status_bar/background_image.rs
@@ -1,19 +1,9 @@
-use std::{
-    sync::{
-        atomic::{AtomicBool, Ordering},
-        mpsc::Receiver,
-        Arc,
-    },
-    thread::JoinHandle,
-};
-
 use macroquad::{
     color::Color,
     math::Vec2,
     texture::{draw_texture_ex, Image, Texture2D},
 };
-use tracing::{debug, error, info, span, trace, warn, Level};
-use url::Url;
+use tracing::{debug, error, info, span, trace, Level};
 
 use crate::config;
 
@@ -21,24 +11,15 @@ use super::StatusBarItem;
 
 pub struct BackgroundImage {
     image: Option<Texture2D>,
-    rx: Receiver<Option<Vec<u8>>>,
-    thread: JoinHandle<()>,
-    should_stop: Arc<AtomicBool>,
     opacity: u8,
 }
 
 impl Default for BackgroundImage {
     fn default() -> Self {
-        let (_, rx) = std::sync::mpsc::channel();
-        let s = Self {
+        Self {
             image: None,
-            rx,
-            thread: std::thread::spawn(|| {}),
-            should_stop: Arc::new(AtomicBool::new(false)),
             opacity: 255,
-        };
-        while !s.thread.is_finished() {}
-        s
+        }
     }
 }
 
@@ -66,56 +47,23 @@ impl StatusBarItem for BackgroundImage {
             return;
         }
 
-        if !self.thread.is_finished() {
-            self.should_stop
-                .store(true, std::sync::atomic::Ordering::Relaxed);
-            warn!("Background image thread has been signalled to stop");
-            return;
-        }
-
         let location = status_bar.buffer.trim().to_string();
 
-        trace!("Attempting to locate: {}...", location);
-        if let Ok(url) = Url::parse(&location) {
-            trace!("Location appears to be a URL");
-            match url.scheme() {
-                "http" | "https" => {
-                    let (tx, rx) = std::sync::mpsc::channel();
-                    self.rx = rx;
-                    let should_stop = Arc::clone(&self.should_stop);
-                    self.thread = std::thread::spawn(move || {
-                        let Ok(req) = reqwest::blocking::get(&location) else {
-                            error!("Request failed");
-                            let _ = tx.send(None);
-                            return;
-                        };
-                        let Ok(data) = req.bytes() else {
-                            error!("Failed to get request bytes");
-                            let _ = tx.send(None);
-                            return;
-                        };
-                        if !should_stop.load(Ordering::Relaxed) {
-                            debug!("Downloaded resource from: {}", location);
-                            let _ = tx.send(Some(data.to_vec()));
-                        }
-                    });
+        if let Some(file) = config::get_file(&location, None) {
+            debug!("Found file '{}', size = {}", location, file.len());
+            match Image::from_file_with_format(&file, None) {
+                Ok(image) => {
+                    let texture = Texture2D::from_image(&image);
+                    self.image = Some(texture);
+                    info!("Loaded texture");
                 }
-                x => {
-                    error!("Scheme is not HTTP/S: {}", x);
-                    status_bar.buffer = "BadScheme".to_string();
+                Err(e) => {
+                    error!("Image is not valid: {}", e);
                 }
-            }
+            };
         } else {
-            trace!("Treating location as file...");
-            if let Some(file) = config::get_file(&location, None) {
-                debug!("Found file '{}', size = {}", location, file.len());
-                let (tx, rx) = std::sync::mpsc::channel();
-                self.rx = rx;
-                let _ = tx.send(Some(file));
-            } else {
-                status_bar.buffer = "FileNotFound".to_string();
-                error!("Could not find file: '{}'", location);
-            }
+            status_bar.buffer = "FileNotFound".to_string();
+            error!("Could not find file: '{}'", location);
         }
     }
 
@@ -126,19 +74,6 @@ impl StatusBarItem for BackgroundImage {
     ) -> (String, macroquad::prelude::Color) {
         let span = span!(Level::INFO, "BackgroundImageUpdate");
         let _enter = span.enter();
-
-        if let Ok(Some(data)) = self.rx.try_recv() {
-            match Image::from_file_with_format(&data, None) {
-                Ok(image) => {
-                    let texture = Texture2D::from_image(&image);
-                    self.image = Some(texture);
-                    info!("Loaded texture");
-                }
-                Err(e) => {
-                    error!("Image is not valid: {}", e);
-                }
-            };
-        }
 
         (
             "".to_string(),
