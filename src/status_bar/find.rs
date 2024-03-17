826dfbad345d1d398a9c4df5bc6113a38595f917
Jaycadox <68955944+Jaycadox@users.noreply.github.com>
2024-03-17 22:04:33 +1000

    Add wrapping behaviour to Find

--- src/status_bar/find.rs
+++ src/status_bar/find.rs
@@ -20,19 +20,28 @@ impl StatusBarItem for Find {
         let _enter = span.enter();
 
         let mut buffer = status_bar.buffer.clone();
+        let size = game.cells.shape()[1];
+        let mut cursor_pos = game.selected_cell.map(|x| SudokuGame::xy_pos_to_idx(x.0, x.1, size as u32)).unwrap_or(0);
+        let pos_auto_set = game.selected_cell.is_none();
+
         let mut direction = FindDirection::Ahead;
         if buffer.len() == 2 {
             if buffer.chars().next() == Some('.') {
                 direction = FindDirection::Behind;
                 buffer.remove(0);
+                if pos_auto_set {
+                    cursor_pos = SudokuGame::xy_pos_to_idx((size - 1) as u32, (size - 1) as u32, size as u32);
+                }
             }
         }
 
-        if let Some(target) = buffer.chars().next().and_then(|x| x.to_digit(10)) {
-            let size = game.cells.shape()[1];
-            let cursor_pos = game.selected_cell.map(|x| SudokuGame::xy_pos_to_idx(x.0, x.1, size as u32)).unwrap_or(0);
+        let fallback_start_pos = match direction {
+            FindDirection::Ahead => 0,
+            FindDirection::Behind => game.cells.len(),
+        };
 
-            let Some((x, y)) = find(game, target as u8, cursor_pos, direction) else {
+        if let Some(target) = buffer.chars().next().and_then(|x| x.to_digit(10)) {
+            let Some((x, y)) = find(game, target as u8, cursor_pos, direction, !pos_auto_set).or_else(|| find(game, target as u8, fallback_start_pos as u32, direction, false))  else {
                 status_bar.buffer = String::from("Could not find instance of character");
                 return;
             };
@@ -49,6 +58,7 @@ impl StatusBarItem for Find {
     }
 }
 
+#[derive(Clone, Copy)]
 enum FindDirection {
     Ahead,
     Behind
@@ -63,11 +73,11 @@ impl FindDirection {
     }
 }
 
-fn find(game: &mut SudokuGame, target: u8, start: u32, direction: FindDirection) -> Option<(u32, u32)> {
+fn find(game: &mut SudokuGame, target: u8, start: u32, direction: FindDirection, has_defined_cursor: bool) -> Option<(u32, u32)> {
     let offset = direction.to_offset();
     let mut i: i64 = start as i64;
 
-    if i != 0 {
+    if !has_defined_cursor {
         i += offset as i64;
     }
 
